cac73d81a6f9d2acbb2502354c909c12b660b7bf
Rayna Auerbach <a.rayna@wustl.edu>
2025-04-01 16:07:56 -0500

    when you click on the move on the side it tells you whose turn it was at the top of the page

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,7 @@ function Board({xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
@@ -66,8 +67,10 @@ export default function Game() {
   }
 
   function jumpTo(nextMove) {
-    // TODO
-  }
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+    }
+    
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
@@ -76,7 +79,7 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
